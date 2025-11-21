0239850317eb5248eb94bcbfd3240ca7dc582411
bilal <bilalahmedkhan52542@gmail.com>
2025-11-21 23:02:13 +0500

    Add files via upload

--- main.asm
+++ main.asm
@@ -8,14 +8,22 @@ INCLUDE Irvine32.inc
 	choice   db ?
 	ext_msg  db "Exiting the game !",0
 	inv_msg  db "Invalid Input !",0
+	player1_board db 100 dup('~')
+	player2_board db 100 dup('~')
+	player3_board db 100 dup('~')
+	player4_board db 100 dup('~')
+	ship1 db ?
+	ship2 db ?
+	ship3 db ?
+	ship4 db 2 dup(?)
 .code
-main PROC
+main proc
 	;menu ko print ka loop hai
 	menu_loop:
 		mov edx,OFFSET menu_msg
-		call WriteString
-		call ReadChar
-		call Crlf
+		call writestring
+		call readchar
+		call crlf
 
 		cmp al,'1' ;starts
 		je start_game
@@ -25,17 +33,55 @@ main PROC
 
 		jmp invalid_input
 	start_game:
-		call Crlf
+		call StartGame
+		call clrscr
+		call crlf
 		jmp menu_loop
 	invalid_input:
-		mov edx,OFFSET inv_msg
-		call WriteString
-		call Crlf
+		mov edx,offset inv_msg
+		call writestring
+		call crlf
 		jmp menu_loop 
 	exit_game:
-		mov edx,OFFSET ext_msg
-		call WriteString
-		call Crlf
+		mov edx,offset ext_msg
+		call writestring
+		call crlf
 		exit
-main ENDP
+main endp
+
+StartGame proc
+	;game start ho toh draw grid aur ask player to input ships
+	call crlf
+	push offset player1_board
+	call DrawGrid
+	call PlaceShips
+	ret
+StartGame endp
+DrawGrid proc 
+	; simple draw grid function which draws which ever grid tha in offset
+	mov esi,[esp+4]
+	mov ecx,10
+	row_loop:
+		push ecx
+		mov ecx,10
+		col_loop:
+			mov al,[esi]
+			call writechar
+			inc esi
+			mov al,' '
+			call writechar
+		loop col_loop
+		call crlf
+		pop ecx
+	loop row_loop
+	ret 4
+DrawGrid endp
+PlaceShips proc
+	; ask for coordinates (using msg) and then validate then place ship :cry:
+	call readint
+	call writedec
+	call readint
+	call writedec
+	ret
+PlaceShips endp
 END main
